be76398e4d20f0a478e766b5b6acb94e8527b88a
siyarampatel <siyarampateljbp@gmail.com>
2026-09-25 16:18:43 +0530

    sliding Q

---
+++ array/maxConsecutive1.java
@@ -0,0 +1,28 @@
+
+class maxConsecutive1 {
+    public static int longestOnes(int[] nums, int k) {
+        int left = 0;
+        int zeroes = 0;
+        int maxLen = 0;
+       
+        for(int right=0; right<nums.length; right++){
+            if(nums[right]==0){
+                zeroes++;
+            }
+            
+            while(zeroes>k){
+                if(nums[left]==0){
+                    zeroes--;
+                }
+                left++;
+            }
+           maxLen = Math.max(maxLen,right-left+1);
+        }
+        return maxLen;
+    }
+    public static void main(String [] args){
+        int nums [] = {1,1,1,0,0,0,1,1,1,1,0};
+        int k = 2;
+        System.out.print(longestOnes(nums,k));
+    }
+}
